de22b798d8885dc03c931118b806429ae499fc2e
wymany <wuyun@joboto.com>
2016-05-30 11:48:19 +0800

    Initial Commit

---
+++ AlgorithmDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AlgorithmDemo
+//
+//  Created by wymany on 16/5/30.
+//  Copyright © 2016年 wymany. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AlgorithmDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  AlgorithmDemo
+//
+//  Created by wymany on 16/5/30.
+//  Copyright © 2016年 wymany. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
